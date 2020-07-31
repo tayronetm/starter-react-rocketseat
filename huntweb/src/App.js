eabaf344efc96587bc239b116dd5e73fdc1ae915
Tayrone <jose.tayrone@ctis.com.br>
2020-07-31 20:53:40 -0300

    Basic configs and Header component

---
+++ huntweb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/header/header-component';
+import './styles.css'
+
+// STATELESS COMPONENT
+const App = () => (
+    <div className="App">
+			<Header />
+    </div>
+  );
+
+
+export default App;
---
+++ huntweb/src/components/header/header-component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header-styles.css'
+
+// STATELESS COMPONENT
+const Header = () => (
+	<div>
+		<header id="main-header">JSHunt</header>
+	</div>
+);
+
+export default Header;
