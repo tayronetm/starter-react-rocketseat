011e68643c083c4e08fab41425c2f1533f1756e7
Tayrone <jose.tayrone@ctis.com.br>
2020-07-31 21:48:45 -0300

    Styles

--- src/pages/main/main-component.js
+++ src/pages/main/main-component.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import api from '../../services/api'
+import api from '../../services/api';
+
+import './main-styles.css';
 
 export default class Main extends Component {
 	// ARMAZENAR ESTADO DO COMPONENTE
@@ -19,10 +21,16 @@ export default class Main extends Component {
 	}
 
 	render() {
+		// BUSCA A VARIAVEL PRODUCTS DENTRO DO STATE
+		const { products } = this.state;
 		return (
 			<div className="product-list">
-				{this.state.products.map(product => (
-					<h2 key={product._id}>{ product.title }</h2>
+				{ products.map(product => (
+					<article key={ product._id }>
+						<strong>{ product.title }</strong>
+						<p>{ product.description }</p>
+						<a href={ product.url }>Acessar</a>
+					</article>
 				))}
 			</div>
 		)
