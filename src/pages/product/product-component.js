f79e550f1883e4fd809aa9343c9b0801f3dfdf86
Tayrone <jose.tayrone@ctis.com.br>
2020-08-01 11:46:00 -0300

    Product Deatails and get param routes

--- src/pages/product/product-component.js
+++ src/pages/product/product-component.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import api from '../../services/api'
+import './product-styles.css'
 
 export default class Product extends Component {
+	state = {
+		product: {}
+	};
+
+	async componentDidMount() {
+		// ACESSAR PARAMETROS DA ROTA
+		const { id } = this.props.match.params;
+		const response = await api.get(`/products/${id}`);
+
+		this.setState({ product: response.data });
+		console.log(this.props);
+	}
+
+	previousPage = () => {
+		const { history } = this.props
+		history.goBack();
+	}
+
 	render() {
-		return <h1>Product</h1>
+		const { product } = this.state;
+
+		return (
+			<div className="product-info">
+				<h1>{ product.title }</h1>
+				<p>{ product.description }</p>
+
+				<p>
+					URL: <a href={ product.url }>{ product.url}</a>
+				</p>
+				<button onClick={ this.previousPage } >Voltar</button>
+			</div>
+
+		)
 	}
 }
